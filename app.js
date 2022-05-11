b40098846844dea2cb5dd13a0a1845815d29723d
tgkcapture <71700867+TgkCapture@users.noreply.github.com>
2022-05-11 14:57:45 +0200

    DB connection

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 const express = require('express');
+const dotenv = require('dotenv');
 
 
 
 const app = express();
 
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+// app.get('/', (req, res) => {
+//     res.send('Hello World!');
+// });
 
-app.get('/api/customers', (req, res) => {
-    res.send([1, 2, 3]);
-});
+// app.get('/api/customers', (req, res) => {
+//     res.send([1, 2, 3]);
+// });
+
+const connectDB = require('./config/db');
+
+dotenv.config({ path: './config/config.env' });
+
+connectDB();
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
